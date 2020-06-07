45ef80fd9860471a69fef08066f7bcf6650e7f01
“lizizi” <929204520@qq.com>
2020-06-07 10:49:59 +0800

    功能扩展 :仿淘宝搜索记录,增加折叠功能,点击箭头显示全部

--- app/src/main/java/com/example/administrator/flowlayout/FlowLayout.java
+++ app/src/main/java/com/example/administrator/flowlayout/FlowLayout.java
@@ -3,9 +3,14 @@ package com.example.administrator.flowlayout;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.nfc.Tag;
 import android.os.Build;
 
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.TypedValue;
@@ -34,7 +39,9 @@ public class FlowLayout extends ViewGroup {
 
     private OnItemClickListener onItemClickListener;
 
-    private int lineSpae; //行间距
+    private int defaultShowCount; //默认显示个数 若为0则显示全部
+
+    private int lineSpace; //行间距
     private int leftMargin; //左间距
     private int rightMargin; //右间距
     private float corner; //圆角 单位为dp
@@ -61,7 +68,7 @@ public class FlowLayout extends ViewGroup {
 
     private void getAttributeSet(Context context, AttributeSet attrs) {
         TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.FlowLayout);
-        lineSpae = (int) array.getDimension(R.styleable.FlowLayout_lineSpace, Utils.dp2px(getContext(), 10));
+        lineSpace = (int) array.getDimension(R.styleable.FlowLayout_lineSpace, Utils.dp2px(getContext(), 10));
         corner = (int) array.getDimension(R.styleable.FlowLayout_flowTextCorner, Utils.dp2px(getContext(), 5));
         leftMargin = (int) array.getDimension(R.styleable.FlowLayout_flowTextLeftMargin, Utils.dp2px(getContext(), 10));
         rightMargin = (int) array.getDimension(R.styleable.FlowLayout_flowTextRightMargin, Utils.dp2px(getContext(), 10));
@@ -72,6 +79,7 @@ public class FlowLayout extends ViewGroup {
         paddingRight = (int) array.getDimension(R.styleable.FlowLayout_flowTextPaddingRight, Utils.dp2px(getContext(), 10));
         paddingTop = (int) array.getDimension(R.styleable.FlowLayout_flowTextPaddingTop, Utils.dp2px(getContext(), 5));
         paddingBottom = (int) array.getDimension(R.styleable.FlowLayout_flowTextPaddingBottom, Utils.dp2px(getContext(), 5));
+        defaultShowCount = array.getInteger(R.styleable.FlowLayout_default_show_count, 0);
         array.recycle();
     }
 
@@ -122,7 +130,7 @@ public class FlowLayout extends ViewGroup {
 
                     //1、记录当前行的最大宽度，高度累加
                     measuredWith = Math.max(measuredWith, currentLineWidth);
-                    measuredHeight = (int) (measuredHeight + currentLineHeight + lineSpae);
+                    measuredHeight = (int) (measuredHeight + currentLineHeight + lineSpace);
                     //2、将当前行的viewList添加至总的mViewsList，将行高添加至总的行高List
                     mViewLinesList.add(viewList);
                     mLineHeights.add(currentLineHeight);
@@ -148,7 +156,7 @@ public class FlowLayout extends ViewGroup {
                 if (i == childCount - 1) { //如果是最后一个子view,不管需不需要换行,都需要把信息存储到集合中
                     //1、记录当前行的最大宽度，高度累加
                     measuredWith = Math.max(measuredWith, currentLineWidth);
-                    measuredHeight = (int) (measuredHeight + currentLineHeight + lineSpae);
+                    measuredHeight = (int) (measuredHeight + currentLineHeight + lineSpace);
 
                     //2、将当前行的viewList添加至总的mViewsList，将行高添加至总的行高List
                     mViewLinesList.add(viewList);
@@ -186,7 +194,7 @@ public class FlowLayout extends ViewGroup {
                 curLeft += childView.getMeasuredWidth() + layoutParams.leftMargin + layoutParams.rightMargin;
             }
             curLeft = getPaddingLeft(); //重置左边距
-            curTop += mLineHeights.get(i) + lineSpae;
+            curTop += mLineHeights.get(i) + lineSpace;
         }
         mViewLinesList.clear();
         mLineHeights.clear();
@@ -195,28 +203,93 @@ public class FlowLayout extends ViewGroup {
     public void setData(List<String> data) {
         this.strings = data;
         if (strings != null && !strings.isEmpty()) {
-            for (int i = 0; i < strings.size(); i++) {
-                FlowTextView flowTextView = new FlowTextView(getContext());
-                flowTextView.setText(strings.get(i));
-                flowTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, flowTextSize);
-                flowTextView.setTextColor(flowTextColor);
-                flowTextView.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
-                flowTextView.setCorner((int) corner);
-                flowTextView.setBackgroundColor(flowTextBgColor);
-                flowTextView.setIncludeFontPadding(false);
-                flowTextView.setGravity(Gravity.CENTER);
-                final int finalI = i;
-                flowTextView.setOnClickListener(new OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        if (onItemClickListener != null) {
-                            onItemClickListener.onItemClick(v, finalI);
-                        }
+            Log.d(TAG, "setData: data.size=" + strings.size());
+            int maxCount;
+            if (defaultShowCount > 0) {
+                maxCount = Math.min(defaultShowCount, strings.size());
+            } else {
+                maxCount = strings.size();
+            }
+
+            for (int i = 0; i < maxCount; i++) {
+                setNormalFlowText(i);
+            }
+            if (defaultShowCount > 0 && defaultShowCount < strings.size()) {
+                setArrowFlowText(maxCount);
+            }
+        }
+        Log.d(TAG, "setData: 数据为空");
+    }
+
+    /**
+     * 设置普通FlowText
+     *
+     * @param i 索引
+     */
+    private void setNormalFlowText(int i) {
+        FlowTextView flowTextView = new FlowTextView(getContext());
+        flowTextView.setText(strings.get(i));
+        flowTextView.setTextSize(TypedValue.COMPLEX_UNIT_PX, flowTextSize);
+        flowTextView.setTextColor(flowTextColor);
+        setFlowText(i, flowTextView);
+    }
+
+    /**
+     * 设置为箭头的flowtext 点击可以展开
+     *
+     * @param maxCount flowtext最大个数
+     */
+    private void setArrowFlowText(final int maxCount) {
+        FlowTextView flowTextView = new FlowTextView(getContext());
+        SpannableString arrowSpan = new SpannableString("h");
+        Drawable drawable = getResources().getDrawable(R.mipmap.arrow_32x32);
+        drawable.setBounds(0, 0, drawable.getMinimumWidth(), (int) flowTextSize);
+        ImageSpan imageSpan = new ImageSpan(drawable, ImageSpan.ALIGN_BOTTOM);
+        arrowSpan.setSpan(imageSpan, 0, 1, Spanned.SPAN_INCLUSIVE_INCLUSIVE);
+        flowTextView.setTag("arrow");
+        flowTextView.setText(arrowSpan);
+        setFlowText(maxCount, flowTextView);
+    }
+
+    /**
+     * 设置通用属性
+     *
+     * @param index        索引
+     * @param flowTextView view
+     */
+    private void setFlowText(final int index, FlowTextView flowTextView) {
+        flowTextView.setPadding(paddingLeft, paddingTop, paddingRight, paddingBottom);
+        flowTextView.setCorner((int) corner);
+        flowTextView.setBackgroundColor(flowTextBgColor);
+        flowTextView.setIncludeFontPadding(false);
+        flowTextView.setGravity(Gravity.CENTER);
+        flowTextView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClick(v, index);
+                    if ("arrow".equals(v.getTag())) {
+                        showAllFlowTextViews(index, v);
                     }
-                });
-                MarginLayoutParams marginLayoutParams = new MarginLayoutParams(MarginLayoutParams.WRAP_CONTENT, MarginLayoutParams.WRAP_CONTENT);
-                marginLayoutParams.setMargins(leftMargin, 0, rightMargin, 0);
-                addView(flowTextView, marginLayoutParams);
+                }
+            }
+        });
+        MarginLayoutParams marginLayoutParams = new MarginLayoutParams(MarginLayoutParams.WRAP_CONTENT, MarginLayoutParams.WRAP_CONTENT);
+        marginLayoutParams.setMargins(leftMargin, 0, rightMargin, 0);
+        addView(flowTextView, marginLayoutParams);
+    }
+
+    /**
+     * 显示全部flowtextview
+     *
+     * @param index 展示剩余flowtextview的起始索引
+     * @param v     flowtextview
+     */
+    private void showAllFlowTextViews(int index, View v) {
+        removeView(v);
+        if (index < strings.size() && index > 0) {
+            for (int i = index; i < strings.size(); i++) {
+                setNormalFlowText(i);
             }
         }
     }
